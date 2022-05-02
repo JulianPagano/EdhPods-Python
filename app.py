beaa6478724e75e57f8e0a17a1411637c21c7dc8
Julian Pagano <julianmgs@gmail.com>
2022-05-02 00:00:22 -0300

    Devolver json. Web no funcionando?

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
 from flask import Flask, render_template, request, jsonify
-from datetime import datetime
 
 app = Flask(__name__)
+
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/createPods", methods=["POST"])
-def createPods():
-    
-    players = request.form.getlist('arrPlayers[]')
-    return jsonify(randomize(players))
+def generate():
+    players = request.json["players"] #.getlist('arrPlayers[]')
+    x = randomize(players)
+    return jsonify(x)
+
 
 def randomize(players):
     import random
     import math
     
+    with open("config.json", "r") as jsonfile:
+        pod_names = json.load(jsonfile)["pod_names"]
+    
+    random.shuffle(pod_names)
     random.shuffle(players)
 
-    numOfPlayers = len(players)
-    numOfTables = math.ceil(numOfPlayers / 4)
+    num_of_pods = math.ceil(len(players) / 4)
 
-    table = 0
+    pod = 0
     spot = 0
-    pods = [[0 for x in range(4)] for y in range(numOfTables)]
+    pods = []
+    pods = [[0 for x in range(4)] for y in range(num_of_pods)]
     for p in players:
-        pods[table][spot] = p
+        pods[pod][spot] = p
 
-        if (table < numOfTables - 1):
-            table += 1
+        if (pod < num_of_pods - 1):
+            pod += 1
         else:
-            table = 0
+            pod = 0
             spot += 1
 
-    return pods
+    pods_json = []
+    for i in range(len(pods)):
+        if 0 in pods[i]: pods[i].remove(0)
+        p = {
+            "pod_name": pod_names[i],
+            "players": pods[i]
+        }
+        pods_json.append(p)
+
+    return list(pods_json)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
